5dd79b04a554b944f04bb3a75e82552dd8f1d568
Thomas Kirkman-Wood <tom@tkw.bz>
2024-02-10 21:12:59 +0000

    Initial Commit

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+author 'TK Studios'
+description 'TK Studios - Public props Library by NuK3.'
+url 'https://buy.tkstudios.store'
+version '1.0.3'
+
+fx_version 'cerulean'
+game {'gta5'}
+lua54 'yes'
+
+dependencies {
+    "tk-lib"
+}
+
+server_script 'server/index.js'
+
+data_file 'DLC_ITYP_REQUEST' 'stream/SaleSigns/SaleSigns.ytyp'
---
+++ server/index.js
@@ -0,0 +1,9 @@
+const Lib = global.exports["tk-lib"].GetLib();
+const Logger = Lib.Functions.Logger("n3-props", "Main")
+
+function init() {
+    Lib.Functions.VersionChecker(GetCurrentResourceName(), Lib.Functions.GetResourceVersion(GetCurrentResourceName()));
+    Lib.Functions.EnsureResourceName(GetCurrentResourceName(), 'n3-props');
+}
+
+init()
